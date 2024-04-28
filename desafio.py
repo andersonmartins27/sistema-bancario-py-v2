bed07ede3a0b3f4bbec96bee919d17e2ee3b376e
Anderson Martins <anderson.martins214@gmail.com>
2024-04-28 15:04:45 -0300

    Add Versão Final do Desafio Proposto

--- desafio.py
+++ desafio.py
@@ -13,22 +13,24 @@ def menu():
     answer = input(menu)
     return answer
 
+
 def sacar(*, saldo, valor, extrato, limite, numero_saques, limite_saque):
     if numero_saques < limite_saque:
-            if valor > saldo:
-                print("Não será possível realizar a operação. Saldo insuficiente!")
-            elif valor > 0:
-                if valor <= limite:
-                    saldo -= valor
-                    numero_saques += 1
-                    extrato += f"Saque de R$ {valor:.2f}\n"
-                elif valor > limite:
-                    print(f"Não é possível realizar saque de valor maior que {limite} por operação!") 
-                else:
-                    print("Não é possível realizar saque de valores negativos!")
+        if valor > saldo:
+            print("Não será possível realizar a operação. Saldo insuficiente!")
+        elif valor > 0:
+            if valor <= limite:
+                saldo -= valor
+                numero_saques += 1
+                extrato += f"Saque de R$ {valor:.2f}\n"
+            elif valor > limite:
+                print(f"Não é possível realizar saque de valor maior que {limite} por operação!")
             else:
-                print(f"Não é possível realizar mais do que {limite_saque} por dia!")
-    return (saldo,extrato)
+                print("Não é possível realizar saque de valores negativos!")
+        else:
+            print(f"Não é possível realizar mais do que {limite_saque} por dia!")
+    return (saldo, extrato)
+
 
 def depositar(saldo, valor, extrato, /):
     if valor >= 0:
@@ -37,7 +39,8 @@ def depositar(saldo, valor, extrato, /):
     else:
         print("Não é possível depositar valores negativos!")
 
-    return (saldo,extrato)
+    return (saldo, extrato)
+
 
 def mostrar_extrato(saldo, /, *, extrato):
     print("\n****************EXTRATO***************")
@@ -45,9 +48,46 @@ def mostrar_extrato(saldo, /, *, extrato):
     print(f"Saldo: R$ {saldo:.2f}")
     print("**************************************")
 
-# def criar_cliente():
 
-# def criar_conta():
+def criar_cliente(clientes):
+    cpf = input("Informe o número do CPF: ")
+    cliente = filtrar_usuario(cpf, clientes)
+
+    if cliente:
+        print(f"Cliente com CPF {cpf} já está cadastrado!")
+        return
+
+    nome = input("Informe o nome completo: ")
+    nascimento = input("Informe a data de nascimento (dd-mm-aaaa): ")
+    endereco = input(
+        "Informe o endereço (logradouro, n° bairro - cidade/uf): ")
+    clientes.append({"nome": nome, "nasc": nascimento,
+                    "cpf": cpf, "endr": endereco})
+    print("Cliente cadastrado com sucesso!\n")
+
+
+def filtrar_usuario(cpf, clientes):
+    clientes_filtrados = [
+        cliente for cliente in clientes if cliente["cpf"] == cpf]
+    return clientes_filtrados[0] if clientes_filtrados else None
+
+
+def criar_conta(agencia, numero_conta, clientes):
+    cpf = input("Informe o número do CPF: ")
+    cliente = filtrar_usuario(cpf, clientes)
+
+    if cliente:
+        print("Conta criada com sucesso!\n")
+        return {"agencia": agencia, "numero_conta": numero_conta, "cliente": cliente}
+    
+    print("Cliente não encontrado! Fluxo de criação de conta encerrado...\n")
+
+
+def listar_contas(contas):
+    for conta in contas:
+        print(f"Agência: {conta["agencia"]}")
+        print(f"Número da Conta: {conta["numero_conta"]}")
+        print(f"Cliente: {conta["cliente"]["nome"]}")
 
 
 def main():
@@ -80,11 +120,20 @@ def main():
 
         elif opcao == "e":
             mostrar_extrato(saldo, extrato=extrato)
-        
+        elif opcao == "nc":
+            numero_conta = len(contas) + 1
+            conta = criar_conta(AGENCIA, numero_conta, clientes)
+
+            if conta:
+                contas.append(conta)
+
+        elif opcao == "lc":
+            listar_contas(contas)
+        elif opcao == "ncl":
+            criar_cliente(clientes)
         elif opcao == "q":
             break
         else:
             print("Operação inválida, por favor selecione novamente a operação desejada.")
-
-
+    
 main()
